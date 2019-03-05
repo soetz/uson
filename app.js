498d5139276d436a27f1e08b7556023103819f3f
soetz <simon.lecutiez@gmail.com>
2019-03-05 02:31:21 +0100

    Added MongoDB connection

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-var express = require('express');
-var app = express();
-
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('app.properties');
 
 //property getter - makes it easy to get a specific app property
 getProp = function(property, def = null) {
   var prop = properties.get(property);
-  if(prop === null) {
+  if(prop === null) { //because properties.get(property) returns null if the property does not exist
     prop = def;
   }
   return prop;
 };
 
-//root endpoint
-app.get("/", function(req, res) {
-  //wip
-});
+getMongoUrl = function() {
+  return 'mongodb://' +
+  getProp('mongo.address', 'localhost') +
+  ':' +
+  getProp('mongo.port', 27017) +
+  '/' +
+  getProp('mongo.database', 'uson');
+}
 
-app.listen(getProp('server.port', 80), function() {
-  //good news: we are live ! let's tell the console
-  console.log(getProp('app.name', 'Ultra Simple Online Notepad') + ' is listening on port ' + getProp('server.port', 80));
+var express = require('express');
+var app = express();
+
+var mongoose = require('mongoose');
+//connect to the mongoose database
+mongoose.connect(getMongoUrl(), {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error: '));
+db.once('open', function() { //we have to wait for the MongoDB connection to open before doing stuff - we won't go anywhere without it anyway
+  //log that, please
+  console.log('The MongoDB connection to the ' + getProp('mongo.database', 'uson') + ' database at ' + getProp('mongo.address', 'localhost') + ' on port ' + getProp('mongo.port', 27017) + ' is now open.');
+
+  //root endpoint
+  app.get("/", function(req, res) {
+    //wip
+    res.send();
+  });
+
+  app.listen(getProp('server.port', 80), function() {
+    //good news: we are live ! let's tell the console
+    console.log(getProp('app.name', 'Ultra Simple Online Notepad') + ' is listening on port ' + getProp('server.port', 80) + '.');
+  });
 });
