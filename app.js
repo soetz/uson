141e4d279848319ede1481d9bd9427d25acf081d
soetz <simon.lecutiez@gmail.com>
2019-03-05 01:35:29 +0100

    Added default parameter to getProp()

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('app.properties');
 
 //property getter - makes it easy to get a specific app property
-getProp = function(property) {
-  return properties.get(property);
+getProp = function(property, def = null) {
+  var prop = properties.get(property);
+  if(prop === null) {
+    prop = def;
+  }
+  return prop;
 };
 
 //root endpoint
@@ -14,7 +18,7 @@ app.get("/", function(req, res) {
   //wip
 });
 
-app.listen(getProp('server.port'), function() {
+app.listen(getProp('server.port', 80), function() {
   //good news: we are live ! let's tell the console
-  console.log(getProp('app.name') + ' is listening on port ' + getProp('server.port'));
+  console.log(getProp('app.name', 'Ultra Simple Online Notepad') + ' is listening on port ' + getProp('server.port', 80));
 });
